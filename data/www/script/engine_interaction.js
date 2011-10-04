e388188d789749b42539756079a06efa50cffb76
Erik Hazzard <enoex1@gmail.com>
2011-10-03 22:18:21 -0400

    Added websocketio test code

--- data/www/script/engine_interaction.js
+++ data/www/script/engine_interaction.js
@@ -639,4 +639,42 @@ window.addEvent('domready', function(e){
 
     //Setup canvas stuff
     VASIR_ENGINE.canvas.functions.init();
+
+    //------------------------------------------------------------------------
+    //
+    //Web Socket stuff
+    //
+    //------------------------------------------------------------------------
+   // Create SocketIO instance, connect
+/*
+    var socket = new io.Socket('localhost',{
+      port: 8080
+    });
+    socket.connect(); 
+
+    // Add a connect listener
+    socket.on('connect',function() {
+      console.log('Client has connected to the server!');
+    });
+    // Add a connect listener
+    socket.on('message',function(data) {
+      console.log('Received a message from the server!',data);
+    });
+    // Add a disconnect listener
+    socket.on('disconnect',function() {
+      console.log('The client has disconnected!');
+    });
+
+    // Sends a message to the server via sockets
+    function sendMessageToServer(message) {
+      socket.send(message);
+    } 
+*/
+
+ var socket = io.connect('http://localhost:8080');
+  socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
+
 });
