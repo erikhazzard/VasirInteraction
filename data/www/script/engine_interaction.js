4e880991cfa4980629983c340ea6f6139218b3c0
Erik Hazzard <enoex1@gmail.com>
2011-10-05 22:00:50 -0400

    minor tweak

--- data/www/script/engine_interaction.js
+++ data/www/script/engine_interaction.js
@@ -671,10 +671,11 @@ window.addEvent('domready', function(e){
     } 
 */
 
+    /*
  var socket = io.connect('http://localhost:8080');
   socket.on('news', function (data) {
     console.log(data);
     socket.emit('my other event', { my: 'data' });
   });
-
+    */
 });
